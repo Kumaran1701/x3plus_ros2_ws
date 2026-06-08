2d3a76d7b332f5e93fee6a61b5d2fa9421b80ec8
Kumaran1701 <kumarang1701@gmail.com>
2026-06-08 11:37:59 +0200

    update 2026-06-08_11:37:59

--- x3plus_camera_arm/x3plus_camera_arm/circle_detector.py
+++ x3plus_camera_arm/x3plus_camera_arm/circle_detector.py
@@ -2,6 +2,8 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import PointStamped
+from tf2_ros import Buffer, TransformListener
 
 import cv2
 from cv_bridge import CvBridge
@@ -14,7 +16,16 @@ class x3plusCircleDetector(Node):
 
         self.depth_frame_flag = False
 
+        self.fx = 543.2409
+        self.fy = 543.2409
+        self.cx0 = 323.0906
+        self.cy0 = 241.3283
+
         self.bridge = CvBridge()
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        
         self.camera_arm_pub_ = self.create_publisher(Image, '/camera_arm/image_circle', 10)
         self.depth_camera_pub_ = self.create_publisher(Image, '/camera_arm/depth_image', 10)
         #self.camera_arm_sub_ = self.create_subscription(Image, '/camera_arm/image_raw', self.image_callback, 10)
@@ -29,7 +40,8 @@ class x3plusCircleDetector(Node):
 
         if not self.depth_frame_flag:
             return
-        
+        point = PointStamped()
+
         frame = self.bridge.imgmsg_to_cv2(msg)
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -80,6 +92,33 @@ class x3plusCircleDetector(Node):
         output_depth.header.frame_id = msg.header.frame_id
 
         circle_distance = self.depth_frame[cy, cx]
+
+        u = cx
+        v = cy
+        z = circle_distance
+
+        x = (u - self.cx0) * z / self.fx
+        y = (v - self.cy0) * z / self.fy
+
+        point.header.stamp = msg.header.stamp
+        point.header.frame_id = "camera_color_optical_frame"
+
+        point.point.x = x
+        point.point.y = y
+        point.point.z = z
+
+        point_base = self.tf_buffer.transform(point, "base_link")
+
+        self.get_logger().info(
+            f"Camera: ({x:.3f}, {y:.3f}, {z:.3f})"
+        )
+
+        self.get_logger().info(
+            f"Base: ({point_base.point.x:.3f}, "
+            f"{point_base.point.y:.3f}, "
+            f"{point_base.point.z:.3f})"
+        )
+
         
 
         self.camera_arm_pub_.publish(output)
