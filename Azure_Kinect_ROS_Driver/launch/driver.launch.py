83248e62b44016863e51f2a7436521792c1229d4
Kumaran1701 <kumarang1701@gmail.com>
2026-08-20 16:04:07 +0200

    update 2026-08-20_16:04:07

--- Azure_Kinect_ROS_Driver/launch/driver.launch.py
+++ Azure_Kinect_ROS_Driver/launch/driver.launch.py
@@ -59,7 +59,7 @@ def generate_launch_description():
         description="Enable or disable the depth camera"),
     DeclareLaunchArgument(
         'depth_mode',
-        default_value="WFOV_UNBINNED",
+        default_value="NFOV_UNBINNED",
         description="Set the depth camera mode, which affects FOV, depth range, and camera resolution. See Azure Kinect documentation for full details. Valid options: NFOV_UNBINNED, NFOV_2X2BINNED, WFOV_UNBINNED, WFOV_2X2BINNED, and PASSIVE_IR"),
     DeclareLaunchArgument(
         'depth_unit',
@@ -75,11 +75,11 @@ def generate_launch_description():
         description="The format of RGB camera. Valid options: bgra, jpeg"),
     DeclareLaunchArgument(
         'color_resolution',
-        default_value="1536P",
+        default_value="720P",
         description="Resolution at which to run the color camera. Valid options: 720P, 1080P, 1440P, 1536P, 2160P, 3072P"),
     DeclareLaunchArgument(
         'fps',
-        default_value="5",
+        default_value="15",
         description="FPS to run both cameras at. Valid options are 5, 15, and 30"),
     DeclareLaunchArgument(
         'point_cloud',
@@ -87,11 +87,11 @@ def generate_launch_description():
         description="Generate a point cloud from depth data. Requires depth_enabled"),
     DeclareLaunchArgument(
         'rgb_point_cloud',
-        default_value="true",
+        default_value="false",
         description="Colorize the point cloud using the RBG camera. Requires color_enabled and depth_enabled"),
     DeclareLaunchArgument(
         'point_cloud_in_depth_frame',
-        default_value="false",
+        default_value="true",
         description="Whether the RGB pointcloud is rendered in the depth frame (true) or RGB frame (false). Will either match the resolution of the depth camera (true) or the RGB camera (false)."),
     DeclareLaunchArgument( # Not a parameter of the node, rather a launch file parameter
         'required',
@@ -110,33 +110,9 @@ def generate_launch_description():
         default_value="false",
         description="If set to true the recording file will rewind the beginning once end of file is reached"),
     DeclareLaunchArgument(
-        'body_tracking_enabled',
-        default_value="false",
-        description="If set to true the joint positions will be published as marker arrays"),
-    DeclareLaunchArgument(
-        'body_tracking_smoothing_factor',
-        default_value="0.0",
-        description="Set between 0 for no smoothing and 1 for full smoothing"),
-    DeclareLaunchArgument(
-        'rescale_ir_to_mono8',
-        default_value="false",
-        description="Whether to rescale the IR image to an 8-bit monochrome image for visualization and further processing. A scaling factor (ir_mono8_scaling_factor) is applied."),
-    DeclareLaunchArgument(
-        'ir_mono8_scaling_factor',
-        default_value="1.0",
-        description="Scaling factor to apply when converting IR to mono8 (see rescale_ir_to_mono8). If using illumination, use the value 0.5-1. If using passive IR, use 10."),
-    DeclareLaunchArgument(
         'imu_rate_target',
         default_value="0",
         description="Desired output rate of IMU messages. Set to 0 (default) for full rate (1.6 kHz)."),
-    DeclareLaunchArgument(
-        'wired_sync_mode',
-        default_value="0",
-        description="Wired sync mode. 0: OFF, 1: MASTER, 2: SUBORDINATE."),
-    DeclareLaunchArgument(
-        'subordinate_delay_off_master_usec',
-        default_value="0",
-        description="Delay subordinate camera off master camera by specified amount in usec."),
     launch_ros.actions.Node(
         package='azure_kinect_ros_driver',
         executable='node',
@@ -155,13 +131,7 @@ def generate_launch_description():
             {'sensor_sn': launch.substitutions.LaunchConfiguration('sensor_sn')},
             {'recording_file': launch.substitutions.LaunchConfiguration('recording_file')},
             {'recording_loop_enabled': launch.substitutions.LaunchConfiguration('recording_loop_enabled')},
-            {'body_tracking_enabled': launch.substitutions.LaunchConfiguration('body_tracking_enabled')},
-            {'body_tracking_smoothing_factor': launch.substitutions.LaunchConfiguration('body_tracking_smoothing_factor')},
-            {'rescale_ir_to_mono8': launch.substitutions.LaunchConfiguration('rescale_ir_to_mono8')},
-            {'ir_mono8_scaling_factor': launch.substitutions.LaunchConfiguration('ir_mono8_scaling_factor')},
-            {'imu_rate_target': launch.substitutions.LaunchConfiguration('imu_rate_target')},
-            {'wired_sync_mode': launch.substitutions.LaunchConfiguration('wired_sync_mode')},
-            {'subordinate_delay_off_master_usec': launch.substitutions.LaunchConfiguration('subordinate_delay_off_master_usec')}]),
+            {'imu_rate_target': launch.substitutions.LaunchConfiguration('imu_rate_target')},]),
     # If flag overwrite_robot_description is set:
     launch_ros.actions.Node(
         package='robot_state_publisher',
