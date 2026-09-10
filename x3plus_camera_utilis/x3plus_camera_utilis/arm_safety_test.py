15364d6e535aed42065b6f9c4cb3966b7d31a0db
Kumaran1701 <kumarang1701@gmail.com>
2026-09-10 14:08:44 +0200

    update 2026-09-10_14:08:44

--- x3plus_camera_utilis/x3plus_camera_utilis/arm_safety_test.py
+++ x3plus_camera_utilis/x3plus_camera_utilis/arm_safety_test.py
@@ -137,8 +137,10 @@ class ArmSafetyTest(Node):
         self.index = 0
 
         # SAFE timing
+        #self.dt = max(self.times[1] - self.times[0], 0.05)  # at least 50 ms
+        #self.run_time_ms = int(self.dt * 1000) - 10         # finish before next command
         self.dt = max(self.times[1] - self.times[0], 0.05)  # at least 50 ms
-        self.run_time_ms = int(self.dt * 1000) - 10         # finish before next command
+        self.run_time_ms = int(self.dt * 1000) + 10
 
         self.get_logger().info(f"SHORT TRAJECTORY TEST — {len(self.times)} waypoints")
         self.get_logger().info(f"dt={self.dt:.3f}s, run_time={self.run_time_ms}ms")
